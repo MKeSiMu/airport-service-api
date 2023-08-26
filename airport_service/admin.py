814889bfb689d88008d9c32c8905da9461cea99b
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-08-26 15:26:57 +0800

    Implemented Flight, Ticket, Order models + add to admin

--- airport_service/admin.py
+++ airport_service/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 
-from airport_service.models import AirplaneType, Airplane, Crew, Airport, Route
+from airport_service.models import (
+    AirplaneType,
+    Airplane,
+    Crew,
+    Airport,
+    Route,
+    Ticket,
+    Order,
+    Flight
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
 
 admin.site.register(AirplaneType)
 admin.site.register(Airplane)
 admin.site.register(Crew)
 admin.site.register(Airport)
 admin.site.register(Route)
+admin.site.register(Flight)
--- airport_service/models.py
+++ airport_service/models.py
@@ -6,6 +6,8 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.utils.text import slugify
 
+from airport_service_api import settings
+
 
 class AirplaneType(models.Model):
     name = models.CharField(
@@ -84,3 +86,78 @@ class Route(models.Model):
 
     def __str__(self):
         return f"{self.source.closest_big_city} - {self.destination.closest_big_city}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, related_name="flights", on_delete=models.CASCADE)
+    airplane = models.ForeignKey(Airplane, related_name="flights", on_delete=models.CASCADE)
+    crew = models.ManyToManyField(Crew, related_name="flights")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["departure_time"]
+
+    def __str__(self):
+        return f"{self.route} ({self.departure_time})"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, related_name="tickets", on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, related_name="tickets", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["row", "seat", "flight"]
+        ordering = ["row", "seat"]
+
+    def __str__(self):
+        return f"{self.flight} - (row: {self.row}; seat: {self.seat})"
+
+    @staticmethod
+    def validate_seat(row: int, seat: int, rows: int, seats_in_row: int, error_to_raise):
+        if not (1 <= seat <= seats_in_row):
+            raise error_to_raise({
+                "seat": f"seat must be in range [1, {seats_in_row}], not {seat}"
+            })
+        if not (1 <= row <= rows):
+            raise error_to_raise({
+                "row": f"row must be in range [1, {rows}], not {row}"
+            })
+
+    def clean(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_field=None
+    ):
+        Ticket.validate_seat(
+            self.row,
+            self.seat,
+            self.flight.airplane.rows,
+            self.flight.airplane.seats_in_row,
+            ValidationError
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_field=None
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(force_insert, force_update, using, update_field)
