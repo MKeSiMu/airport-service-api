72653b65b8557d41692817f261a3ce3228395426
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-08-26 11:12:41 +0800

    Added image field to Airplane model

--- airport_service/models.py
+++ airport_service/models.py
@@ -1,4 +1,8 @@
+import os
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
 
 
 class AirplaneType(models.Model):
@@ -14,15 +18,22 @@ class AirplaneType(models.Model):
         return f"{self.id}: {self.name}"
 
 
+def airplane_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+
 class Airplane(models.Model):
     name = models.CharField(max_length=65, unique=True)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
     airplane_type = models.ForeignKey(
-        AirplaneType,
-        on_delete=models.CASCADE,
-        related_name="airplanes"
+        AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
     )
+    image = models.ImageField(null=True, upload_to=airplane_image_file_path)
 
     class Meta:
         ordering = ["name"]
